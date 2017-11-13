78a782611c4c67f43959170b24e4cb244d2a1ba1
Victor Hugo Possidonio <vtrhp@hotmail.com>
2017-11-13 21:01:38 -0200

    Metodo remove funcionando, foi necessário alterar o ProdutoBean para SessionScoped

---
+++ src/br/com/caelum/notasfiscais/mb/NotaFiscalBean.java
@@ -0,0 +1,87 @@
+package br.com.caelum.notasfiscais.mb;
+
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.caelum.notasfiscais.dao.NotaFiscalDao;
+import br.com.caelum.notasfiscais.dao.ProdutoDao;
+import br.com.caelum.notasfiscais.modelo.Item;
+import br.com.caelum.notasfiscais.modelo.NotaFiscal;
+import br.com.caelum.notasfiscais.modelo.Produto;
+
+@Named
+@ViewScoped
+public class NotaFiscalBean {
+	private NotaFiscal notaFiscal = new NotaFiscal();
+	
+	private Item item = new Item();
+	private Long idProduto;
+	
+	@Inject
+	private ProdutoDao produtoDao;
+	
+	public void guardaItem(){
+		Produto produto = produtoDao.buscaPorId(idProduto);
+		item.setProduto(produto);
+		item.setValorUnitario(produto.getPreco());
+		
+		notaFiscal.getItens().add(item);
+		item.setNotaFiscal(notaFiscal);
+		
+		item = new Item();
+		idProduto = null;
+	}
+	
+	
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public ProdutoDao getProdutoDao() {
+		return produtoDao;
+	}
+
+	public void setProdutoDao(ProdutoDao produtoDao) {
+		this.produtoDao = produtoDao;
+	}
+
+	public NotaFiscalDao getNotaFiscalDao() {
+		return notaFiscalDao;
+	}
+
+	public void setNotaFiscalDao(NotaFiscalDao notaFiscalDao) {
+		this.notaFiscalDao = notaFiscalDao;
+	}
+
+	public void setNotaFiscal(NotaFiscal notaFiscal) {
+		this.notaFiscal = notaFiscal;
+	}
+
+	@Inject
+	private NotaFiscalDao notaFiscalDao;
+	
+	public void gravar(){
+		this.notaFiscalDao.adiciona(notaFiscal);
+		
+		this.notaFiscal = new NotaFiscal();
+	}
+	
+	public NotaFiscal getNotaFiscal(){
+		return notaFiscal;
+	}
+
+}
--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,8 +1,10 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -10,12 +12,26 @@ import br.com.caelum.notasfiscais.dao.ProdutoDao;
 import br.com.caelum.notasfiscais.modelo.Produto;
 
 @Named
-@RequestScoped
-public class ProdutoBean {
+@SessionScoped
+public class ProdutoBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	private Produto produto = new Produto();
 	@Inject
 	private ProdutoDao dao;
 	
+	/*public ProdutoDao getDao() {
+		return dao;
+	}
+
+	public void setDao(ProdutoDao dao) {
+		this.dao = dao;
+	}*/ //teste metodo remove
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
@@ -49,10 +65,9 @@ public class ProdutoBean {
 		
 	}
 	
-	public void remove(){				
-		dao.remove(produto);		
-		produtos = dao.listaTodos();	
-		this.produto = new Produto();
+	public void remove(Produto produto){				
+		this.dao.remove(produto);		
+		this.produtos = dao.listaTodos();				
 	}
 
 }

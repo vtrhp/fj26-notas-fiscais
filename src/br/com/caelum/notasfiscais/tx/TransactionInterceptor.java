5e7c288de2e814022da7707b73490444288593b9
Victor Hugo Possidonio <vtrhp@hotmail.com>
2017-11-16 19:16:51 -0200

    Ocorre erro ao adicionar um item.

---
+++ src/br/com/caelum/notasfiscais/tx/TransactionInterceptor.java
@@ -0,0 +1,33 @@
+package br.com.caelum.notasfiscais.tx;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+
+@Interceptor
+@Transactional
+public class TransactionInterceptor implements Serializable{
+	
+	@Inject
+	private EntityManager manager;
+	
+	@AroundInvoke
+	public Object intercept(InvocationContext ctx) throws Exception{
+		System.out.println("Abrindo a transação");
+		
+		manager.getTransaction().begin();
+		
+		Object resultado = ctx.proceed();
+		
+		manager.getTransaction().commit();
+		System.out.println("Comitando a transação");
+		
+		return resultado;
+		
+	}
+
+}
---
+++ src/br/com/caelum/notasfiscais/tx/ViewModel.java
@@ -0,0 +1,10 @@
+package br.com.caelum.notasfiscais.tx;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public @interface ViewModel {
+
+}
